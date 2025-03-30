bb705bfe14b5450ab81224cae3f3964f0187f837
jacobthomasjohnson <jacobjohnson@email.com>
2025-03-30 01:01:45 -0500

    First Form & Function Commit

---
+++ app/components/Header.js
@@ -0,0 +1,9 @@
+export const Header = () => {
+      return (
+            <header id="header" className="flex justify-center w-[90%] mx-auto py-8 pb-4">
+                  <div className="text-lg font-bold">Form & Function</div>
+            </header>
+      )
+}
+
+export default Header;
---
+++ app/components/QuickStats.js
@@ -0,0 +1,20 @@
+export const QuickStats = () => {
+      return (
+            <>
+                  <div className="flex justify-between p-6 border rounded-sm border-[#3E3E3E] mb-4">
+                        <div>Money</div>
+                        <div>$837</div>
+                  </div>
+                  <div className="flex justify-between p-6 border rounded-sm border-[#3E3E3E] mb-4">
+                        <div>Job Title</div>
+                        <div>Intern</div>
+                  </div>
+                  <div className="flex justify-between p-6 border rounded-sm border-[#3E3E3E] mb-4">
+                        <div>Auto Process</div>
+                        <div>1 P/s</div>
+                  </div>
+            </>
+      )
+}
+
+export default QuickStats;
